3838c4b049867b9b2556a87f263d642ff01b3299
Julien Enoch <946462+JEnoch@users.noreply.github.com>
2019-09-05 16:54:28 +0200

    Call eval callback in a go routine (to allow the callback query Yaks)

--- examples/y_register_eval/y_register_eval.go
+++ examples/y_register_eval/y_register_eval.go
@@ -8,16 +8,6 @@ import (
 	"github.com/atolab/yaks-go"
 )
 
-func eval(path *yaks.Path, props yaks.Properties) yaks.Value {
-	fmt.Println(props)
-	name := props["name"]
-	if name == "" {
-		name = "World"
-	}
-
-	return yaks.NewStringValue("Hello " + name + "!")
-}
-
 func main() {
 	locator := "tcp/127.0.0.1:7447"
 	if len(os.Args) > 1 {
@@ -38,6 +28,26 @@ func main() {
 	fmt.Println("Use Workspace on '/'")
 	root, _ := yaks.NewPath("/")
 	w := y.Workspace(root)
+
+	eval := func(path *yaks.Path, props yaks.Properties) yaks.Value {
+		fmt.Println(props)
+		name := props["name"]
+		if name == "" {
+			name = "World"
+		}
+
+		// if name starts with '/' consider it as a path to get the real name to use
+		if name[0] == '/' {
+			s, err := yaks.NewSelector(name)
+			if err == nil {
+				kvs := w.Get(s)
+				if len(kvs) > 0 {
+					name = kvs[0].Value().ToString()
+				}
+			}
+		}
+		return yaks.NewStringValue("Hello " + name + "!")
+	}
 
 	fmt.Println("Register eval " + p.ToString())
 	err = w.RegisterEval(p, eval)
